3d1a69d8ef014d0ef99d7a3f02419ca868550a19
Nihar Patel <patelnihar14@gmail.com>
2024-01-07 20:17:10 +0530

    .

--- Task/Reduce.js
+++ Task/Reduce.js
@@ -1,15 +1,12 @@
-
-let arr = [1, 2, 3, 4, 5]
+let arr = [1, 2, 3, 4, 5];
 
 let find = arr.reduce((lastEle, ele) => {
+  console.log("LastElement is: ", lastEle);
+  console.log("Element Is: ", ele);
 
-    console.log("LastElement is: ", lastEle);
-    console.log("Element Is: ", ele);
-
-    return lastEle + ele;
-})
+  return lastEle + ele;
+}, 10);
 
 console.log("Reduce:", find);
-
 
 //reduce print return value....
--- Task/Spread.js
+++ Task/Spread.js
@@ -1,18 +1,16 @@
-
-let arr = [1, 2, 3, 4, 5, 6, 7]
-let arr2 = arr;  // Refrence...
-let arr3 = [...arr, 10];  // Copy..
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let arr2 = arr; // Refrence...
+let arr3 = [...arr, 10]; // Copy..
 
 console.log("Array:", arr2);
 
 console.log("Array3:", arr3);
 
-
 let obj1 = { a: 10, b: 5 };
 
-let obj2 = obj1;  //Reference of object..
+let obj2 = obj1; //Reference of object..
 
-let obj3 = { ...obj1 };  // Copy of object.. 
+let obj3 = { ...obj1 }; // Copy of object..
 
 obj1.a = 20;
 
