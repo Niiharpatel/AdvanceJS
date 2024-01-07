38d59ea0237a2fd4b45c73814a39a12bacc55f54
Nihar Patel <patelnihar14@gmail.com>
2024-01-07 20:17:21 +0530

    .

--- Task/Promise_all.js
+++ Task/Promise_all.js
@@ -1,21 +1,18 @@
-
-
 let pr1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log("Hello user this is promise 1");
-        resolve("Hello user this is promise 1");
-
-    }, 4000);
-})
+  setTimeout(() => {
+    console.log("Hello user this is promise 1");
+    resolve("Hello user this is promise 1");
+  }, 4000);
+});
 
 let pr2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log("Hello user this is promise 2");
-        resolve("Hello user this is promise 2");
+  setTimeout(() => {
+    console.log("Hello user this is promise 2");
+    resolve("Hello user this is promise 2");
+  }, 3000);
+});
 
-    }, 3000);
-})
-
-Promise.all([pr1, pr2]).then(function (values) {  // promise.all wait for both promises to b complete and then print at same time without any delay.
-    console.log(values);
-})
+Promise.all([pr1, pr2]).then(function (values) {
+  // promise.all wait for both promises to b complete and then print at same time without any delay.
+  console.log(values);
+});
--- Task/Recursion.js
+++ Task/Recursion.js
@@ -1,4 +1,3 @@
-
 // function loop(x) {
 //     console.log(x);
 //     if (x > 0) {
@@ -10,19 +9,10 @@
 
 // loop(3);
 
-
-function count(num) {
-
-
-    console.log(num);  //Display the number..
-
-
-    const total = num - 1; // Decrease the num value..
-
-
-    if (total > 0) {
-        count(total);   // Recursion...
-    }
-}
-
-count(5); //  Call the function for get answer..
+let decrementCounter = (number) => {
+  // Base case condition....
+  if (number === 0) return;
+  console.log(number);
+  decrementCounter(number - 1);
+};
+decrementCounter(5);
